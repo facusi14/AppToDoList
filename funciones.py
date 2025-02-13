db9c08e151536ed67a0c2f3b91dd4bd62e23bbb7
Facundo <facundogomez1455@gmail.com>
2025-02-13 01:35:47 -0300

    Terminando funcion mostrar y empezando a validar

--- funciones.py
+++ funciones.py
@@ -1,34 +1,53 @@
 tareas = []
 
 def agregar_tarea():
-    nombre_tarea = input("Ingrese el nombre de la tarea")
+    nombre_tarea = input(" \nIngrese el nombre de la tarea:  ")
     while nombre_tarea == "":
         print("El nombre de la tarea no puede estar vacio")
         nombre_tarea = input("Ingrese nuevamente el nombre de la tarea")
         
     tareas.append({"nombre": nombre_tarea, "completada":False})
-    print(f"Tarea '{nombre_tarea}' agregada correctamente.")    
+    print( "\n--------------")
+    print(f" \n La Tarea \n {nombre_tarea} fue agregada correctamente ")    
+    print( "\n")
         
-def mostrar_tareas():
+def mostrar_tareas():   
     contador = 0
+    
     for i in tareas:
-        contador += 1
-        print(f"La tarea {contador} es {i}")
+        contador_tareas_pendientes = 0
+        if i["completada"] == False:
+            contador_tareas_pendientes += 1
+            print(f"Las tareas que estan pendientes  son {i["nombre"]}")
+        elif i["completada"]  == True:
+            print(f"Las tareas que estan completadas son {i["nombre"]}")
+        
+            
+        
+        
 
 def marca_completada():
-    indice = int(input("Ingrese el indice de la tarea que quiere marcar como completada"))
-    indice = indice - 1
-    while indice < 0 or indice >= len(tareas):
-        print(f"Ingrese un indice correcto")
+    
+    try:
         indice = int(input("Ingrese el indice de la tarea que quiere marcar como completada"))
+        indice = indice - 1
+        while indice < 0 or indice >= len(tareas):
+            print(f"Ingrese un indice correcto")
+            indice = int(input("Ingrese el indice de la tarea que quiere marcar como completada"))
         
-    tarea_marcada = tareas[indice]
-    tarea_marcada["completada"] = True
-    print(f"La tarea {tarea_marcada} ha sido marcada como completada")
+        tarea_marcada = tareas[indice]
+        tarea_marcada["completada"] = True
+        
+        print(f"La tarea {tarea_marcada} ha sido marcada como completada")
+        
+    except ValueError:
+        print("Error debe ingresar un indice numero entero valido")
+    
             
         
     
     while indice > len(tareas):
+        
         print("Error el indice no esta disponible, reingrese nuevamente un indice valido")
         indice = int(input("Ingrese el indice de la tarea que quiere marcar como completada"))
     
